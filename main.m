9f67d722d4c04055aead62c5030ce2e4becd1bdc
He Jiang <jiangherenren@163.com>
2026-02-24 14:43:18 +0800

    Update main.m

--- main.m
+++ main.m
@@ -18,16 +18,16 @@ for ii = 1:LengthFiles
         outimg1=image(:,:,1);
         outimg2=image(:,:,2);
         outimg3=image(:,:,3);
-        %% get H1 
+        % get H1 
         H1_outimg1=PBRI(outimg1);             
         H1_outimg2=PBRI(outimg2);
         H1_outimg3=PBRI(outimg3);
-        %% get details
+        % get details
         Details=zeros(size(image,1),size(image,2),3);
         Details(:,:,1)=imresize(H1_outimg1,[size(image,1),size(image,2)],'bilinear');
         Details(:,:,2)=imresize(H1_outimg2,[size(image,1),size(image,2)],'bilinear');
         Details(:,:,3)=imresize(H1_outimg3,[size(image,1),size(image,2)],'bilinear');
-        %% add details to the original images
+        % add details to the original images
         outimg1=outimg1+Details(:,:,1)*factor;
         outimg2=outimg2+Details(:,:,2)*factor;
         outimg3=outimg3+Details(:,:,3)*factor;
@@ -38,10 +38,10 @@ for ii = 1:LengthFiles
     if d == 1
         outimg_temp=image;
         H_outimg_temp=PCRH(outimg_temp);             
-        %% get details
+        % get details
         Details=zeros(size(image,1),size(image,2),1);
         Details=imresize(H_outimg_temp,[size(image,1),size(image,2)],'bilinear');
-        %% add details to the original images
+        % add details to the original images
         outimg1=outimg1+Details*factor;
         outimg=outimg1;
     end
@@ -50,6 +50,7 @@ for ii = 1:LengthFiles
     toc
 
 end
+
 
 
 
